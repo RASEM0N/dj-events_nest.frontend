661bc8a8bb958c05c84843156f36568e95a4c9cc
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-05-23 00:04:33 +0500

    create: event page & fetch event

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -1,12 +1,82 @@
 import React from 'react'
 import Layout from '@/components/Layout'
+import axios from 'axios'
+import { API_URL_EVENTS } from '@/config/API'
+import styles from '@/styles/Event.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const EventPage = () => {
+const EventPage = ({ evt }) => {
+    const deleteEvent = () => {
+        console.log('delete event')
+    }
+
     return (
         <Layout>
-            <h1>My event</h1>
+            <div className={styles.event}>
+                <div className={styles.controls}>
+                    <Link href={`/events/edit/${evt.id}`}>
+                        <a>
+                            {/*<FaPencilAlt /> */}
+                            Edit Event
+                        </a>
+                    </Link>
+                    <a href="#" className={styles.delete} onClick={deleteEvent}>
+                        {/*<FaTimes /> */}
+                        Delete Event
+                    </a>
+                </div>
+
+                <span>
+                    {evt.date} at {evt.time}
+                </span>
+                <h1>{evt.name}</h1>
+                {evt.image && (
+                    <div className={styles.image}>
+                        <Image src={evt.image} width={960} height={600} />
+                    </div>
+                )}
+
+                <h3>Performers:</h3>
+                <p>{evt.performers}</p>
+                <h3>Description:</h3>
+                <p>{evt.description}</p>
+                <h3>Venue: {evt.venue}</h3>
+                <p>{evt.address}</p>
+
+                <Link href="/events">
+                    <a className={styles.back}>{'<'} Go Back</a>
+                </Link>
+            </div>
         </Layout>
     )
 }
 
 export default EventPage
+
+// https://nextjs.org/docs/basic-features/data-fetching#getstaticpaths-static-generation
+export const getStaticPaths = async () => {
+    const data = await axios.get(API_URL_EVENTS).then((resp) => resp.data.data)
+
+    const paths = data.map((evt) => ({
+        params: {
+            slug: evt.slug,
+        },
+    }))
+
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
+export const getStaticProps = async ({ params: { slug } }) => {
+    const data = await axios.get(`${API_URL_EVENTS}/${slug}`).then((resp) => resp.data.data)
+    console.log(slug)
+    return {
+        props: {
+            evt: data[0],
+        },
+        revalidate: 1,
+    }
+}
